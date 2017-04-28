c056bea78e967ce8d527154546ad6fe16ea7bf4c
Adrian Begg <jimmydea@gmail.com>
2017-04-28 13:16:02 +1000

    Updated to add items to a collection for CSV

--- Check-VMWareTools-RSSIncompatibility.ps1
+++ Check-VMWareTools-RSSIncompatibility.ps1
@@ -6,6 +6,8 @@
 # Purpose: A quick script to check if the machines are affected by the bug dscribed in
 # https://blogs.vmware.com/apps/2017/03/rush-post-vmware-tools-rss-incompatibility-issues.html
 #
+# Outputs objects to a CSV at $strCSVOutput
+#
 # The script assumes that all of the machines targeted are ton the same domain for the Invoke-Guest call
 # Use the Get-Cluster/Get-ResourcePool in $vm's to filter
 #
@@ -13,23 +15,31 @@
 # and if the RSS Feature has been enabled on the guestCredentials
 ##########################################################################################
 [string] $strvCenter = "labvc1.pigeonnuggets.com"
+[string] $strCSVOutput = "C:\Temp\RSSIncompatibility.csv"
+
 Connect-VIServer $strvCenter
 
 $vms = Get-VM
 $guestCredentials = Get-Credential
 
 $colVMs = $vms | ?{($_.ExtensionData.Config.Tools.ToolsVersion -gt "9100") -and ($_.ExtensionData.Config.Tools.ToolsVersion -lt "10150") -and (($_ | Get-NetworkAdapter).Type -contains "Vmxnet3") -and (($_.ExtensionData.Guest.GuestId -eq "windows8Server64Guest") -or ($_.ExtensionData.Guest.GuestId -eq "windows9Server64Guest"))}
+# Decalre a collection for storing the StorageProfile Configuarations for each VM
+$colVMSResult = New-Object -TypeName System.Collections.ArrayList
 foreach($vm in $colVMs){
 	# Check if the machine is PoweredOn
 	If($vm.PowerState -eq "PoweredOn"){
 		Try{
 			$RSSEnabled = (Invoke-VMScript -VM $vm -ScriptText "(Get-NetAdapterRss).Enabled" -GuestCredential $guestCredentials -WarningAction SilentlyContinue).ScriptOutput
-			$vm | Select Name,@{name="ToolsVersion"; expression={$_.ExtensionData.Config.Tools.ToolsVersion}}, @{name="Guest"; expression={$_.ExtensionData.Guest.GuestFullName}}, @{name="RSSEnabled"; expression={$RSSEnabled}}
+			$objResult = $vm | Select Name,@{name="ToolsVersion"; expression={$_.ExtensionData.Config.Tools.ToolsVersion}}, @{name="Guest"; expression={$_.ExtensionData.Guest.GuestFullName}}, @{name="RSSEnabled"; expression={$RSSEnabled}}
+			$colVMSResult.Add($objResult) > $nul
 		} Catch {
-			$vm | Select Name,@{name="ToolsVersion"; expression={$_.ExtensionData.Config.Tools.ToolsVersion}}, @{name="Guest"; expression={$_.ExtensionData.Guest.GuestFullName}}, @{name="RSSEnabled"; expression={"Unknown"}}
+			$objResult = $vm | Select Name,@{name="ToolsVersion"; expression={$_.ExtensionData.Config.Tools.ToolsVersion}}, @{name="Guest"; expression={$_.ExtensionData.Guest.GuestFullName}}, @{name="RSSEnabled"; expression={"Unknown"}}
+			$colVMSResult.Add($objResult) > $nul
 		}
 	} else {
-		$vm | Select Name,@{name="ToolsVersion"; expression={$_.ExtensionData.Config.Tools.ToolsVersion}}, @{name="Guest"; expression={$_.ExtensionData.Guest.GuestFullName}}, @{name="RSSEnabled"; expression={"Unknown"}}
+		$objResult = $vm | Select Name,@{name="ToolsVersion"; expression={$_.ExtensionData.Config.Tools.ToolsVersion}}, @{name="Guest"; expression={$_.ExtensionData.Guest.GuestFullName}}, @{name="RSSEnabled"; expression={"Unknown"}}
+		$colVMSResult.Add($objResult) > $nul
 	}
 }
+$colVMSResult | Export-CSV -NoTypeInformation $strCSVOutput
 Disconnect-VIServer
